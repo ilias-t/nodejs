981281079e26d2b8fc168f35bde60d3f62ca8ee6
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-22 22:37:14 -0700

    removes unnecessary files

--- randomizer.js
+++
@@ -1,6 +0,0 @@
-// returns a random number between a min (inclusive) and max (exclusive) range
-function randomizer(min, max) {
-  return parseInt(Math.random() * (max - min) + min);
-}
-
-module.exports = randomizer;
